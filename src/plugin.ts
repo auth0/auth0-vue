5a1f6b11afeac741f1decbb01f3c9cc28d7088b5
Frederik <frederik.prijck@gmail.com>
2022-01-12 14:42:59 +0100

    Update docs for skipRedirectCallback

--- src/plugin.ts
+++ src/plugin.ts
@@ -5,7 +5,26 @@ import version from './version';
 
 export const AUTH0_TOKEN = '$auth0';
 
+/**
+ * Configuration for the Auth0 Vue plugin
+ */
 export interface Auth0PluginOptions extends Auth0ClientOptions {
+  /**
+   * By default, if the page URL has code and state parameters, the SDK will assume they are for
+   * an Auth0 application and attempt to exchange the code for a token.
+   * In some cases the code might be for something else (e.g. another OAuth SDK). In these
+   * instances you can instruct the client to ignore them by setting `skipRedirectCallback`.
+   *
+   * ```js
+   * createAuth0({
+   *   skipRedirectCallback: window.location.pathname === '/other-callback'
+   * })
+   * ```
+   *
+   * **Note**: In the above example, `/other-callback` is an existing route that will be called
+   * by any other OAuth provider with a `code` (or `error` in case when something went wrong) and `state`.
+   *
+   */
   skipRedirectCallback?: boolean;
 }
 
