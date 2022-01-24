3848eb3552f96efb7ffe78d92f8f15c3bdf9d2de
Frederik <frederik.prijck@gmail.com>
2022-01-24 11:14:54 +0100

    Add documentation

--- src/plugin.ts
+++ src/plugin.ts
@@ -32,6 +32,12 @@ export interface Auth0PluginOptions {
    */
   skipRedirectCallback?: boolean;
 
+  /**
+   * The router instance to be used by our SDK when doing a redirect after handling the PKCE flow.
+   *
+   * @remarks
+   * Marked as optional, but required when using routing in your application.
+   */
   router?: Router;
 }
 
