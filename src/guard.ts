610f5f19095f877bdc3fca65060b642343271a2f
Frederik <frederik.prijck@gmail.com>
2022-01-24 10:57:29 +0100

    Return boolean from guard

--- src/guard.ts
+++ src/guard.ts
@@ -1,19 +1,20 @@
 import { App, watchEffect } from 'vue';
+import { RouteLocation } from 'vue-router';
 import { Auth0VueClient } from './client.proxy';
 import { AUTH0_TOKEN } from './token';
 
 export function createAuthGuard(app: App) {
-  return (to: any, from: any, next: Function) => {
+  return (to: RouteLocation) => {
     const auth0 = app.config.globalProperties[AUTH0_TOKEN] as Auth0VueClient;
 
     const fn = () => {
       if (auth0.isAuthenticated.value) {
-        return next();
+        return true;
       }
 
       auth0.loginWithRedirect({ appState: { target: to.fullPath } });
 
-      return next(false);
+      return false;
     };
 
     if (!auth0.isLoading.value) {
