35953023436e7a624e2d6f6de03128d3fa1c9621
Frederik <frederik.prijck@gmail.com>
2022-02-01 14:45:32 +0100

    Rephrase api docs for skipRedirectCallback

--- src/plugin.ts
+++ src/plugin.ts
@@ -15,10 +15,12 @@ export interface Auth0VueClientOptions extends Auth0ClientOptions {}
  */
 export interface Auth0PluginOptions {
   /**
-   * By default, if the page URL has code and state parameters, the SDK will assume they are for
-   * an Auth0 application and attempt to exchange the code for a token.
-   * In some cases the code might be for something else (e.g. another OAuth SDK). In these
-   * instances you can instruct the client to ignore them by setting `skipRedirectCallback`.
+   * By default, if the page URL has code and state parameters, the SDK will assume it should handle it and attempt to exchange the code for a token.
+   *
+   * In situations where you are combining our SDK with other libraries that use the same `code` and `state` parameters,
+   * you will need to ensure our SDK can differentiate between requests it should and should not handle.
+   *
+   * In these cases you can instruct the client to ignore certain URLs by setting `skipRedirectCallback`.
    *
    * ```js
    * createAuth0({
@@ -26,8 +28,8 @@ export interface Auth0PluginOptions {
    * })
    * ```
    *
-   * **Note**: In the above example, `/other-callback` is an existing route that will be called
-   * by any other OAuth provider with a `code` (or `error` in case when something went wrong) and `state`.
+   * **Note**: In the above example, `/other-callback` is an existing route, with a `code` (or `error` in case when something went wrong) and `state`, that will be handled
+   * by any other SDK.
    *
    */
   skipRedirectCallback?: boolean;
