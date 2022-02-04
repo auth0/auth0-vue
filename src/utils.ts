5c246735b2700e69fee66c54e1a11ef00fd9c817
Frederik <frederik.prijck@gmail.com>
2022-02-04 10:12:20 +0100

    Add comments for watchEffectOnce

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,8 @@ import { watchEffect } from 'vue';
 
 /**
  * @ignore
+ * Run watchEffect untill the watcher returns true, then stop the watch.
+ * Once it returns true, the promise will resolve.
  */
 export function watchEffectOnceAsync<T>(watcher: () => T) {
   return new Promise<void>(resolve => {
@@ -11,6 +13,8 @@ export function watchEffectOnceAsync<T>(watcher: () => T) {
 
 /**
  * @ignore
+ * Run watchEffect untill the watcher returns true, then stop the watch.
+ * Once it returns true, it will call the provided function.
  */
 export function watchEffectOnce<T>(watcher: () => T, fn: Function) {
   const stopWatch = watchEffect(() => {
