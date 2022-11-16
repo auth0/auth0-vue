4a4c4bfb09e9e3cb292630e75cbf3b4523ee6e70
Ewan Harris <ewan.harris@okta.com>
2022-11-16 18:25:13 +0000

    Revert back to older cypress version

---
+++ cypress/support/commands.js
@@ -0,0 +1,12 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
