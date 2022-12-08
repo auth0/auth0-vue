a3643f5f943657e9de969548a9e5ad6c0459290d
Ewan Harris <ewan.harris@okta.com>
2022-12-08 13:55:31 +0000

    Export spa-js types, default docs to show inherited and external (#181)

--- src/global.ts
+++ src/global.ts
@@ -1,3 +1,21 @@
 export { Auth0Plugin } from './plugin';
 export * from './interfaces';
 export * from './guard';
+
+export {
+    AuthorizationParams,
+    PopupLoginOptions,
+    PopupConfigOptions,
+    GetTokenWithPopupOptions,
+    LogoutOptions,
+    LogoutUrlOptions,
+    CacheLocation,
+    GetTokenSilentlyOptions,
+    IdToken,
+    User,
+    ICache,
+    InMemoryCache,
+    LocalStorageCache,
+    Cacheable,
+    RedirectLoginOptions,
+  } from '@auth0/auth0-spa-js';
