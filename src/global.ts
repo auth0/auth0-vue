a190599d4f3ab898b88cc8ab9f227038f11e896a
Ewan Harris <ewan.harris@okta.com>
2022-12-08 14:39:11 +0000

    Update spa-js and omit onRedirect from exported types

--- src/global.ts
+++ src/global.ts
@@ -3,19 +3,17 @@ export * from './interfaces';
 export * from './guard';
 
 export {
-    AuthorizationParams,
-    PopupLoginOptions,
-    PopupConfigOptions,
-    GetTokenWithPopupOptions,
-    LogoutOptions,
-    LogoutUrlOptions,
-    CacheLocation,
-    GetTokenSilentlyOptions,
-    IdToken,
-    User,
-    ICache,
-    InMemoryCache,
-    LocalStorageCache,
-    Cacheable,
-    RedirectLoginOptions,
-  } from '@auth0/auth0-spa-js';
+  AuthorizationParams,
+  PopupLoginOptions,
+  PopupConfigOptions,
+  GetTokenWithPopupOptions,
+  LogoutUrlOptions,
+  CacheLocation,
+  GetTokenSilentlyOptions,
+  IdToken,
+  User,
+  ICache,
+  InMemoryCache,
+  LocalStorageCache,
+  Cacheable,
+} from '@auth0/auth0-spa-js';
--- src/interfaces/auth0-vue-client-options.ts
+++ src/interfaces/auth0-vue-client-options.ts
@@ -1,6 +1,14 @@
-import type { Auth0ClientOptions } from '@auth0/auth0-spa-js';
+import type {
+  Auth0ClientOptions,
+  LogoutOptions as SPALogoutOptions,
+  RedirectLoginOptions as SPARedirectLoginOptions
+} from '@auth0/auth0-spa-js';
 
 /**
  * Configuration for the Auth0 Vue Client
  */
 export interface Auth0VueClientOptions extends Auth0ClientOptions {}
+
+export interface LogoutOptions extends Omit<SPALogoutOptions, 'onRedirect'> {}
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface RedirectLoginOptions<TAppState = any> extends Omit<SPARedirectLoginOptions<TAppState>, 'onRedirect'> {}
